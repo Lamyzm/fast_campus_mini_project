15f6fab650726f858971a48d75a4b3f6fa4d9157
hojin <devbang5674@gmail.com>
2024-03-19 18:23:42 +0900

    아이콘 수정

--- src/app/styleguide/page.jsx
+++ src/app/styleguide/page.jsx
@@ -1,7 +1,8 @@
 "use client";
 import { MainContainer } from "@/components/MainContainer";
-import Icons from "@/components/icons";
 import Buttons from "@/components/button/Buttons";
+import IconsList from "@/components/icons";
+
 
 export default function styleguide() {
   return (
@@ -12,22 +13,7 @@ export default function styleguide() {
           {/* size option : small, large
           color option : primary, secondary (상황에 맞게 색상 수정) */}
           <h1> 아이콘 </h1>
-          <Icons type="ArrowBackIcon" size="large" color="primary" />
-          <Icons type="LogoutIcon" size="large" color="primary" />
-          <Icons type="LoginIcon" size="large" color="primary" />
-          <Icons type="SearchIcon" size="large" color="primary" />
-          <Icons type="ArrowForwardIosIcon" size="large" color="primary" />
-          <Icons type="AddIcon" size="large" color="primary" />
-          <Icons type="PermIdentityOutlinedIcon" size="large" color="primary" />
-          <Icons type="AccessTimeIcon" size="large" color="primary" />
-          <Icons type="CheckIcon" size="large" color="primary" />
-          <Icons type="HelpOutlineIcon" size="large" color="primary" />
-          <Icons type="CloseIcon" size="large" color="primary" />
-          <Icons type="ExpandMoreIcon" size="large" color="primary" />
-          <Icons type="ScheduleIcon" size="large" color="primary" />
-          <Icons type="ShoppingCartOutlinedIcon" size="large" color="primary" />
-          <Icons type="HomeOutlinedIcon" size="large" color="primary" />
-          <Icons type="LocationOnOutlinedIcon" size="large" color="primary" />
+          <IconsList />
         </div>
       </MainContainer>
     </>
---
+++ src/components/icons/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Icons from './icons'
+
+const IconsList = () => {
+    return (
+        <>
+            <Icons type="ArrowBackIcon" size="small" color="primary" />
+            <Icons type="LogoutIcon" size="large" color="primary" />
+            <Icons type="LoginIcon" size="large" color="primary" />
+            <Icons type="SearchIcon" size="large" color="primary" />
+            <Icons type="ArrowForwardIosIcon" size="large" color="primary" />
+            <Icons type="AddIcon" size="large" color="primary" />
+            <Icons type="PermIdentityOutlinedIcon" size="large" color="primary" />
+            <Icons type="AccessTimeIcon" size="large" color="primary" />
+            <Icons type="CheckIcon" size="large" color="primary" />
+            <Icons type="HelpOutlineIcon" size="large" color="primary" />
+            <Icons type="CloseIcon" size="large" color="primary" />
+            <Icons type="ExpandMoreIcon" size="large" color="primary" />
+            <Icons type="ScheduleIcon" size="large" color="primary" />
+            <Icons type="ShoppingCartOutlinedIcon" size="large" color="primary" />
+            <Icons type="HomeOutlinedIcon" size="large" color="primary" />
+            <Icons type="LocationOnOutlinedIcon" size="large" color="primary" />
+        </>
+    )
+}
+
+export default IconsList
