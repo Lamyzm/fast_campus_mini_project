ae70901d3250e6f24d0a99d6ea91a9f1d99cb059
hojin <devbang5674@gmail.com>
2024-03-20 17:39:23 +0900

    fit:지역카테고리 디자인 수정

--- src/components/locationCategory/LocationCategory.jsx
+++ src/components/locationCategory/LocationCategory.jsx
@@ -6,18 +6,25 @@ const LocationCategory = () => {
     <SelectLayoutContainer>
       <div className="grid grid-cols-3 border-2 border-solid border-gray-300">
         <div className="w-full h-[162px] flex flex-col items-center justify-center border-2 border-solid border-gray-300 cursor-pointer hover:bg-gray-100 transition duration-300 ease-in-out">
-          <p className="text-3xl font-bold">서울</p>
-          <p className="text-gray-500 mt-2 font-medium">강남 홍대 종로</p>
+          <div className='mb-6'>
+            <div className='w-[90px] text-sm bg-blue-500 rounded-full flex justify-center items-center text-white font-bold mb-1'>일상 속 여유</div>
+            <p className="text-3xl font-bold flex justify-center items-center">서울</p>
+            <p className="text-gray-500 mt-2 font-medium flex justify-center items-center">강남 홍대 종로</p>
+          </div>
+
         </div>
 
         <div className="w-full h-[162px] flex flex-col items-center justify-center border-2 border-solid border-gray-300 cursor-pointer hover:bg-gray-100 transition duration-300 ease-in-out">
-          <p className="text-3xl font-bold">경기</p>
+          <p className=" text-3xl font-bold">경기</p>
           <p className="text-gray-500 mt-2 font-medium">용인 가평 수원</p>
         </div>
 
         <div className="w-full h-[162px] flex flex-col items-center justify-center border-2 border-solid border-gray-300 cursor-pointer hover:bg-gray-100 transition duration-300 ease-in-out">
-          <p className="text-3xl font-bold">강원</p>
-          <p className="text-gray-500 mt-2 font-medium">속초 강릉 춘천</p>
+          <div className='mb-6'>
+            <div className='w-[90px] text-sm bg-blue-500 rounded-full flex justify-center items-center text-white font-bold mb-1'>요즘 뜨는</div>
+            <p className="text-3xl font-bold flex justify-center items-center">강원</p>
+            <p className="text-gray-500 mt-2 font-medium flex justify-center items-center">속초 강릉 춘천</p>
+          </div>
         </div>
 
         <div className="w-full h-[162px] flex flex-col items-center justify-center border-2 border-solid border-gray-300 cursor-pointer hover:bg-gray-100 transition duration-300 ease-in-out">
@@ -41,8 +48,11 @@ const LocationCategory = () => {
         </div>
 
         <div className="w-full h-[162px] flex flex-col items-center justify-center border-2 border-solid border-gray-300 cursor-pointer hover:bg-gray-100 transition duration-300 ease-in-out">
-          <p className="text-3xl font-bold">제주</p>
-          <p className="text-gray-500 mt-2 font-medium">서귀포 성산 애월</p>
+          <div className='mb-6 flex flex-col justify-center items-center'>
+            <div className='w-[90px] text-sm bg-blue-500 rounded-full flex justify-center items-center text-white font-bold mb-1'>언제나 인기</div>
+            <p className="text-3xl font-bold flex justify-center items-center">제주</p>
+            <p className="text-gray-500 mt-2 font-medium flex justify-center items-center">서귀포 성산 애월</p>
+          </div>
         </div>
 
         <div className="w-full h-[162px] flex flex-col items-center justify-center border-2 border-solid border-gray-300 cursor-pointer hover:bg-gray-100 transition duration-300 ease-in-out">
