218d990ea40fa59892575653652d6701201ed3b0
hojin <devbang5674@gmail.com>
2024-03-20 15:01:36 +0900

    feat:인기지역수정

--- src/app/styleguide/page.jsx
+++ src/app/styleguide/page.jsx
@@ -4,10 +4,11 @@ import Buttons from "@/components/buttons/Buttons";
 import AccommodationSwiper from "@/components/accommodationSwipers/AccommodationSwiper";
 import Showcase from "@/components/accommodationSwipers/AccommodationSwiper";
 import IconsList from "@/components/icons";
-import MainSlideContainer from "@/components/mainSlide/MainSlideContainer";
 import { Input } from "@/components/input";
 import Calendar from "@/components/calendar/Calendar";
 import NewCalendar from "@/components/calendar/Calendar2";
+import PopularLocationSlideContainer from "@/components/popularLoactionSlide/popularLocationSlideContainer";
+import CouponSlideContainer from "@/components/couponSlide/CouponSlideContainer";
 
 export default function styleguide() {
   return (
@@ -36,9 +37,10 @@ export default function styleguide() {
           <h1> 아이콘 </h1>
           <IconsList />
         </div>
-        <MainSlideContainer />
+        <PopularLocationSlideContainer />
         <Calendar />
         <NewCalendar />
+        <CouponSlideContainer/>
       </MainContainer>
     </>
   );
---
+++ src/components/couponSlide/CouponSlide.jsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react'
+
+const CouponSlide = () => {
+    return (
+        <div className='flex items-center justify-between mt-5 mb-5'>
+            <div className='mr-10 rounded-xl w-[250px] h-[150]'><img className="rounded-xl w-full object-cover block " src="/couponImages/20000won.png" alt="" /></div>
+            <div className='mr-10 rounded-xl w-[250px] h-[150]'><img className="rounded-xl w-full object-cover block " src="/couponImages/30000won.png" alt="" /></div>
+            <div className='mr-10 rounded-xl w-[250px] h-[150]'><img className="rounded-xl w-full object-cover block " src="/couponImages/50000won.png" alt="" /></div>
+        </div>
+    )
+}
+
+export default CouponSlide
