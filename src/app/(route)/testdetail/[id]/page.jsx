8599cbaafff48fe8bf7b75c457f3e331c0b65d7a
kano <ajvls98@gmail.com>
2024-03-29 16:53:18 +0900

    test: test url로 변경

--- src/app/(route)/testdetail/[id]/page.jsx
+++ src/app/(route)/testdetail/[id]/page.jsx
@@ -1,50 +1,58 @@
-'use client'
-import axios from 'axios';
-import React, { useState } from 'react'
-import { useQuery } from 'react-query';
-import Loader from '../components/Loader';
-import SelectedRoomMain from '@/components/selectedRoomMain/SelectedRoomMain';
-import DetailNav from '@/components/detailNav/DetailNav';
-import SelectedRoomDetailLayout from '@/components/SelectedRoomDetailLayout';
-import RoomDetail from '@/components/roomdetail/RoomDetail';
-import LoadingDetail from '../components/LoadingDetail';
+"use client";
+import axios from "axios";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import Loader from "../components/Loader";
+import SelectedRoomMain from "@/components/selectedRoomMain/SelectedRoomMain";
+import DetailNav from "@/components/detailNav/DetailNav";
+import SelectedRoomDetailLayout from "@/components/SelectedRoomDetailLayout";
+import RoomDetail from "@/components/roomdetail/RoomDetail";
+import LoadingDetail from "../components/LoadingDetail";
 
 const Page = ({ params }) => {
-  const { id } = params
-  const [lat, setLat] = useState(null)
-  const [lng, setLng] = useState(null)
+  const { id } = params;
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
 
   const fetchRoom = async () => {
-    const { data } = await axios(`http://3.35.216.158:8080/api/accommodation/${id}`)
-    return data
-  }
+    const { data } = await axios(
+      `http://183.96.51.234:8080/api/accommodation/${id}`
+    );
+    return data;
+  };
 
   const fetchLocation = (address) => {
-    const axios = require('axios');
+    const axios = require("axios");
     const config = {
-      method: 'get',
+      method: "get",
       maxBodyLength: Infinity,
       url: `https://dapi.kakao.com/v2/local/search/address.json?query=${address}`,
       headers: {
-        'Authorization': 'KakaoAK 87c35d760084065f3b62bab317fdd18b'
+        Authorization: "KakaoAK 87c35d760084065f3b62bab317fdd18b",
       },
     };
-    axios.request(config)
+    axios
+      .request(config)
       .then((response) => {
-        setLng(response.data.documents[0].x)
-        setLat(response.data.documents[0].y)
+        setLng(response.data.documents[0].x);
+        setLat(response.data.documents[0].y);
       })
       .catch((error) => {
         console.log(error);
       });
-  }
+  };
 
-  const { data: room, isFetching, isError, isSuccess, isLoading } = useQuery({
-    queryKey: ['room', id],
+  const {
+    data: room,
+    isFetching,
+    isError,
+    isSuccess,
+    isLoading,
+  } = useQuery({
+    queryKey: ["room", id],
     queryFn: fetchRoom,
-    onSuccess: (data) => fetchLocation(data.address)
-  })
-
+    onSuccess: (data) => fetchLocation(data.address),
+  });
 
   if (isError) {
     return (
@@ -59,14 +67,14 @@ const Page = ({ params }) => {
 
   return (
     <>
-      {isLoading && (<LoadingDetail />)}
+      {isLoading && <LoadingDetail />}
       <DetailNav />
       <SelectedRoomDetailLayout>
         <SelectedRoomMain data={room} />
         <RoomDetail data={room} lat={lat} lng={lng} />
       </SelectedRoomDetailLayout>
     </>
-  )
-}
+  );
+};
 
 export default Page;
