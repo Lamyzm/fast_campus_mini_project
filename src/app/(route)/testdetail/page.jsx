96055d29b584147a14256e8535ec926515fbc84e
kano <ajvls98@gmail.com>
2024-03-29 16:32:15 +0900

    test: mixed contents 테스트

--- src/app/(route)/testdetail/page.jsx
+++ src/app/(route)/testdetail/page.jsx
@@ -1,37 +1,46 @@
-'use client'
-import axios from 'axios';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import AccommodationList from './components/AccommodationList';
-import useIntersectionObserver from './hooks/useIntersectionObserver';
-import Loading from './components/Loading';
-import Loader from './components/Loader';
-import { useInfiniteQuery } from 'react-query';
-import ScrollToTopButton from '@/components/ScrollToTopButton';
+"use client";
+import axios from "axios";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import AccommodationList from "./components/AccommodationList";
+import useIntersectionObserver from "./hooks/useIntersectionObserver";
+import Loading from "./components/Loading";
+import Loader from "./components/Loader";
+import { useInfiniteQuery } from "react-query";
+import ScrollToTopButton from "@/components/ScrollToTopButton";
 
 export default function Home() {
-  const [isEnd, setIsEnd] = useState(false)
-  const ref = useRef()
-  const pageRef = useIntersectionObserver(ref, {})
-  const isPageEnd = !!pageRef?.isIntersecting
+  const [isEnd, setIsEnd] = useState(false);
+  const ref = useRef();
+  const pageRef = useIntersectionObserver(ref, {});
+  const isPageEnd = !!pageRef?.isIntersecting;
   const [showScrollButton, setShowScrollButton] = useState(false);
 
   const fetchRoom = async ({ page }) => {
-    const { data } = await axios.get("http://3.35.216.158:8080/api/accommodation", {
-      params: {
-        page: page
+    const { data } = await axios.get(
+      "http://183.96.51.234:8080/api/accommodation",
+      {
+        params: {
+          page: page,
+        },
       }
-    });
+    );
     return data;
-  }
+  };
 
-  const { data: allRoom, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery({
-    queryKey: ['allRoom'],
+  const {
+    data: allRoom,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+  } = useInfiniteQuery({
+    queryKey: ["allRoom"],
     queryFn: ({ pageParam = 0 }) => fetchRoom({ page: pageParam }),
-    getNextPageParam: (lastPage) => lastPage.last ? undefined : lastPage.number + 1
+    getNextPageParam: (lastPage) =>
+      lastPage.last ? undefined : lastPage.number + 1,
   });
 
   const fetchNext = useCallback(async () => {
-    setIsEnd(false)
+    setIsEnd(false);
     const res = await fetchNextPage();
     if (res.isError) {
       console.log(res.error);
@@ -39,40 +48,40 @@ export default function Home() {
   }, [fetchNextPage]);
 
   useEffect(() => {
-    let timerId
+    let timerId;
     if (isPageEnd && hasNextPage) {
-      setIsEnd(true)
+      setIsEnd(true);
       timerId = setTimeout(() => {
         fetchNext();
       }, 2000);
     }
     return () => clearTimeout(timerId);
-
-  }, [isPageEnd, hasNextPage, fetchNext])
+  }, [isPageEnd, hasNextPage, fetchNext]);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.pageYOffset > 1200) { // 예시로 300px 스크롤됐을 때 버튼을 보이게 설정
+      if (window.pageYOffset > 1200) {
+        // 예시로 300px 스크롤됐을 때 버튼을 보이게 설정
         setShowScrollButton(true);
       } else {
         setShowScrollButton(false);
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <>
-      {isLoading && (<Loading />)}
+      {isLoading && <Loading />}
       {allRoom?.pages.map((page, index) => (
         <React.Fragment key={index}>
           <AccommodationList data={page.content} />
         </React.Fragment>
       ))}
-      {(isEnd) && <Loader />}
-      <div className='w-full touch-none h-10 mb-10' ref={ref} />
+      {isEnd && <Loader />}
+      <div className="w-full touch-none h-10 mb-10" ref={ref} />
       <ScrollToTopButton show={showScrollButton} />
     </>
   );
